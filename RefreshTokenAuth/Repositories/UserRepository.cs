a6ef368e01b5058b6abaafaa5b47da5040661540
Felipe França <felipe.franca@fitbank.com.br>
2022-07-08 16:43:02 -0300

    Add project files.

---
+++ RefreshTokenAuth/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using RefreshTokenAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RefreshTokenAuth.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>
+            {
+                new User {Id = 1, Username = "batman", Password = "batman", Role = "manager"},
+                new User { Id = 1, Username = "batman", Password = "batman", Role = "employer" }
+            };
+
+            return users.FirstOrDefault(x => x.Username == username && x.Password == password);
+        }
+    }
+}
